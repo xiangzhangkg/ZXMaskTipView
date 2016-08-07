f69554e293b92818af57cae467da7abebdee8fae
Zhang, Xiang <xiangzhangkg@126.com>
2016-08-08 00:18:05 +0800

    Init framework

---
+++ ZXMaskTipView/ZXMaskTipView.h
@@ -0,0 +1,19 @@
+//
+//  ZXMaskTipView.h
+//  ZXMaskTipView
+//
+//  Created by Zhang, Xiang(Xiang) on 8/8/16.
+//  Copyright © 2016 xiangzhangkg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZXMaskTipView.
+FOUNDATION_EXPORT double ZXMaskTipViewVersionNumber;
+
+//! Project version string for ZXMaskTipView.
+FOUNDATION_EXPORT const unsigned char ZXMaskTipViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZXMaskTipView/PublicHeader.h>
+
+
